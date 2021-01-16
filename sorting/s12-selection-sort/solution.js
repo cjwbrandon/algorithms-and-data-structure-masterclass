4bdb116afe7b4cf0f1910b57f45bd5198c344ddc
cjwbrandon <cjwbrandon@gmail.com>
2021-01-16 12:34:35 +0800

    [ADD][s12] Selection Sort

---
+++ sorting/s12-selection-sort/solution.js
@@ -0,0 +1,24 @@
+function selectionSort(arr) {
+  let noSwaps, smallest;
+  // Loop through array
+  for (let i = 0; i < arr.length; i++) {
+    noSwaps = true;
+    // 1. Store the first element as the smallest value
+    smallest = i;
+    // Loop through array
+    for (let j = i + 1; j < arr.length; j++) {
+      // if smaller value is found, designate smaller number to be the new minimum
+      if (arr[j] < arr[smallest]) {
+        smallest = j;
+        noSwaps = false;
+      }
+    }
+    if (noSwaps) break;
+    // If minimum is not initial value, swap positions
+    if (smallest !== i) [arr[i], arr[smallest]] = [arr[smallest], arr[i]];
+  }
+
+  return arr;
+}
+
+console.log(selectionSort([6, 3, 7, 5, 4]));
