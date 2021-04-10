9ca8150bc1eabccbc5bc88e65f211fcbafb5504c
cjwbrandon <cjwbrandon@gmail.com>
2021-04-10 15:28:52 +0800

    [ADD][S21] Added stacks

---
+++ data-structures/s21-stacks/main.js
@@ -0,0 +1,40 @@
+// Using Linked List
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.first) {
+      this.first = this.last = newNode;
+    } else {
+      let temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+    }
+    return ++this.size;
+  }
+
+  pop() {
+    if (this.size <= 0) {
+      return null;
+    }
+    let temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+}
